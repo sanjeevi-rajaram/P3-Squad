6a7503154afaee31e4fce66bc5ded77b8dc2c01d
praveenkumar-outlook <praveen.ece@outlook.com>
2018-06-03 21:02:20 +0530

    UI : Blog page

--- src/Components/Contact.react.js
+++ src/Components/Contact.react.js
@@ -1,62 +1,103 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {FBEmbedPost, FBEmbedVideo} from "facebook-plugins";
+import InstagramEmbed from "react-instagram-embed";
 
 class Contact extends React.Component {
   componentDidMount() {
     this.el = $(ReactDOM.findDOMNode(this));
+    this.el.find(".tabs").tabs();
   }
 
   render() {
     return (
       <div className="ui-contact">
         <div className="container">
-          <h4>Contact Us</h4>
           <div className="row">
-            <div className="col s12 m6 l6">
-              <div className="card horizontal small">
-                <div className="card-image">
-                  <img src="images/profile.jpg"/>
-                </div>
-                <div className="card-stacked">
-                  <div className="card-content">
-                    <h5>Mark</h5>
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                      sed do labore et dolore magna aliqua.
-                    </p>
-                  </div>
-                  <div className="card-action">
-                    <a>Phone: +9192432489834</a>
-                  </div>
-                </div>
+            <div className="col s12 m12 l12">
+              <ul className="tabs">
+                <li className="tab col s3 m3 l3">
+                  <a href="#facebook">Facebook</a>
+                </li>
+                <li className="tab col s3 m3 l3">
+                  <a className="active" href="#instagram">Instagram</a>
+                </li>
+                <li className="tab col s3 m3 l3">
+                  <a href="#others">Others</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div id="facebook" className="row">
+            <div className="col s12 m4 l4">
+              <div className="col s12 m12 l12 margin-b-10">
+                <FBEmbedPost appId="383211488859037"
+                  href="https://www.facebook.com/PainPassionProgress/posts/2042252116102041"
+                  width={350}>
+                </FBEmbedPost>
+              </div>
+              <div className="col s12 m12 l12 margin-b-10">
+                <FBEmbedVideo appId="383211488859037"
+                  href="https://www.facebook.com/PainPassionProgress/videos/1813073999019855"
+                  width={350}>
+                </FBEmbedVideo>
+              </div>
+            </div>
+            <div className="col s12 m4 l4">
+              <div className="col s12 m12 l12 margin-b-10">
+                <FBEmbedVideo appId="383211488859037"
+                  href="https://www.facebook.com/PainPassionProgress/videos/2044603772533542"
+                  width={350}>
+                </FBEmbedVideo>
+              </div>
+              <div className="col s12 m12 l12 margin-b-10">
+                <FBEmbedPost appId="383211488859037"
+                  href="https://www.facebook.com/PainPassionProgress/posts/1821603101500278"
+                  width={350}>
+                </FBEmbedPost>
+              </div>
+            </div>
+            <div className="col s12 m4 l4">
+              <div className="col s12 m12 l12 margin-b-10">
+                <FBEmbedPost appId="383211488859037"
+                  href="https://www.facebook.com/PainPassionProgress/posts/2017639208563332"
+                  width={350}>
+                </FBEmbedPost>
+              </div>
+              <div className="col s12 m12 l12 margin-b-10">
+                <FBEmbedPost appId="383211488859037"
+                  href="https://www.facebook.com/PainPassionProgress/posts/1789797978014124"
+                  width={350}>
+                </FBEmbedPost>
               </div>
             </div>
           </div>
-          <h4>Find us at</h4>
-          <div className="row">
-            <div className="col s12 m6 l4">
-              <h5>Eekatuthangal</h5>
-              <p>
-                No 1, Main road,
-                Poomagal street,
-                Eekatuthangal. 600032
-              </p>
+          <div id="instagram" className="row">
+            <div className="col s12 m4 l4">
+              <div className="col s12 m12 l12 margin-b-10">
+                <InstagramEmbed url="https://www.instagram.com/p/Bjg0Bx3HXSm/"
+                  maxWidth={350}>
+                </InstagramEmbed>
+              </div>
             </div>
-            <div className="col s12 m6 l4">
-              <h5>Eekatuthangal</h5>
-              <p>
-                No 1, Main road,
-                Poomagal street,
-                Eekatuthangal. 600032
-              </p>
+            <div className="col s12 m4 l4">
+              <div className="col s12 m12 l12 margin-b-10">
+                <InstagramEmbed url="https://www.instagram.com/p/BjOsFMhHRcS/"
+                  maxWidth={350}>
+                </InstagramEmbed>
+              </div>
             </div>
-            <div className="col s12 m6 l4">
-              <h5>Eekatuthangal</h5>
-              <p>
-                No 1, Main road,
-                Poomagal street,
-                Eekatuthangal. 600032
-              </p>
+            <div className="col s12 m4 l4">
+              <div className="col s12 m12 l12 margin-b-10">
+                <InstagramEmbed url="https://www.instagram.com/p/Bfphczuh1Dp/"
+                  maxWidth={350}>
+                </InstagramEmbed>
+              </div>
+            </div>
+          </div>
+          <div id="others" className="row">
+            <div className="col s12 m12 l12 center-text empty-content">
+              No Content
             </div>
           </div>
         </div>
--- src/Components/Footer.react.js
+++ src/Components/Footer.react.js
@@ -12,25 +12,18 @@ class Footer extends React.Component {
       <footer className="ui-footer page-footer">
         <div className="container">
           <div className="row">
-            <div className="col l6 s12">
+            <div className="col s12 m8 l8">
               <h5>Excaliburs-P3</h5>
-              <p>
-                Indoor GYM regiment of work-outs directed
-              </p>
-              <p>
-                Outdoor special cardio sessions
-              </p>
-              <p>
-                Corporate Fitness Programmes
-              </p>
-              <p>
-                Diabetes fitness training
-              </p>
-              <p>
-                Summer boot camp
-              </p>
+              <div className="col s6 m6 l6">
+                <p>Eekaduthangal</p>
+                <p>Eekaduthangal</p>
+              </div>
+              <div className="col s6 m6 l6">
+                <p>Adyar</p>
+                <p>Adyar</p>
+              </div>
             </div>
-            <div className="col l4 offset-l2 s12">
+            <div className="col s12 m4 l4">
               <h5>Links</h5>
               <div>
                 <FBPage appId="383211488859037"
